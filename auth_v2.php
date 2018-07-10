d9e26a68ecebd60a0b255a6228d6c382c9f15c1c
thiagofaria <thiagotfaria@gmail.com>
2018-07-09 22:53:28 -0300

    Teste1

--- auth_v2.php
+++ auth_v2.php
@@ -16,18 +16,18 @@ $password = remove_junk($_POST['password']);
            updateLastLogIn($user['id']);
            // redirect user to group home page by user level
            if($user['user_level'] === '1'):
-             $session->msg("s", "Hello ".$user['username'].", Welcome to OSWA-INV.");
+             $session->msg("s", "Olá ".$user['username'].", Bem Vindo ao Data Warehouse Monitoring.");
              redirect('admin.php',false);
            elseif ($user['user_level'] === '2'):
-              $session->msg("s", "Hello ".$user['username'].", Welcome to OSWA-INV.");
+              $session->msg("s", "Olá ".$user['username'].", Bem Vindo ao Data Warehouse Monitoring.");
              redirect('special.php',false);
            else:
-              $session->msg("s", "Hello ".$user['username'].", Welcome to OSWA-INV.");
+              $session->msg("s", "Olá ".$user['username'].", Bem Vindo ao Data Warehouse Monitoring.");
              redirect('home.php',false);
            endif;
 
         else:
-          $session->msg("d", "Sorry Username/Password incorrect.");
+          $session->msg("d", "Desculpe, Usuário/Senha Incorreta.");
           redirect('index.php',false);
         endif;
 
